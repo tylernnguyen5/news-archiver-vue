a5c1abc3dfaecd9741cd9fcbda04b8af3f461ec0
Tyler <tyler.nnguyen5@gmail.com>
2020-08-06 16:43:28 +1000

    Added footer component

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="footer">
-    <h1>This is my footer</h1>
-  </div>
+  <footer class="page-footer">
+    <div class="footer container">
+      <!-- FIXME: Add push pull -->
+      <div class="row"> 
+        <div class="col s3"><span class="flow-text">tylernnguyen5</span></div>
+
+        <div class="col s2"><span class="flow-text">
+          <a href="https://www.linkedin.com/in/tyler-nguyen-484721151/">
+            <img height="20" width="20" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" />
+          </a>
+
+          <a href="https://github.com/tylernnguyen5">
+            <img height="20" width="20" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg"  />
+          </a>
+        </span></div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -15,6 +30,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.footer a{
+  filter: invert(96%) sepia(96%) saturate(0%) hue-rotate(66deg) brightness(103%) contrast(107%);
+}
 </style>
